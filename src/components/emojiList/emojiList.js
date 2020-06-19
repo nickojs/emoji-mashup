92131c61c71bb44fad4f3c5616977e7538bb8dad
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-18 21:01:48 -0300

    mocked emojiList

---
+++ src/components/emojiList/emojiList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const EmojiList = (props) => {
+  let a;
+
+  return (
+    <div>
+      <h1>Body part</h1>
+      <ul>
+        <li>img</li>
+        <li>img</li>
+        <li>img</li>
+        <li>img</li>
+        <li>img</li>
+        <li>img</li>
+        <li>img</li>
+        <li>img</li>
+        <li>img</li>
+      </ul>
+    </div>
+  );
+};
+
+export default EmojiList;
