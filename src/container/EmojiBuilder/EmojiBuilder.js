e0cd01823e33cd2cdf2426ca4e86852ef0b91c90
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-23 03:05:32 -0300

    Created reducer to deal with emoji changes

--- src/container/EmojiBuilder/EmojiBuilder.js
+++ src/container/EmojiBuilder/EmojiBuilder.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import EmojiList from '../../components/emojiList/emojiList';
 import assets from '../../assets/mappedEmojiAssets.json';
 
+const initialState = {
+  body: null,
+  effects: null,
+  eyes: null,
+  mouth: null,
+  nose: null
+};
+
+const emojiReducer = (state, action) => {
+  switch (action.type) {
+    case 'BODY':
+      return { ...state, body: action.value };
+    case 'EFFECTS':
+      return { ...state, effects: action.value };
+    case 'EYES':
+      return { ...state, eyes: action.value };
+    case 'MOUTH':
+      return { ...state, mouth: action.value };
+    case 'NOSE':
+      return { ...state, nose: action.value };
+
+    default:
+      throw new Error('reducer error');
+  }
+};
+
 const EmojiBuilder = () => {
   const types = Object.keys(assets);
+  const [state, dispatch] = useReducer(emojiReducer, initialState);
 
   return (
     <div>
@@ -13,6 +40,7 @@ const EmojiBuilder = () => {
           key={type}
           bodypart={type}
           assets={assets[type]}
+          setFinalEmoji={(value) => dispatch({ type: type.toUpperCase(), value })}
         />
       ))}
     </div>
