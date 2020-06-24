05f179f66aa6481fa65fdb8ae90b5df2d5f8bb9d
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-24 03:08:55 -0300

    Passing assets to store when app is first mounted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import assets from './assets/mappedEmojiAssets.json';
 import EmojiBuilder from './container/EmojiBuilder/EmojiBuilder';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log('init app..;');
+    dispatch({ type: 'ASSETS', value: assets });
+  }, [dispatch]);
+
   return (
     <div>
       <EmojiBuilder />
