7afabdcec7c0f27541e61f2d93b5e1be840a54d9
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-24 01:53:45 -0300

    Mocked new emojiList on Bodyparts

--- src/components/bodylist/bodylist.js
+++ src/components/bodylist/bodylist.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './styles';
 
-const Bodylist = ({ types }) => (
-  <S.BodyPartList>
-    {types.map((type) => (
-      <S.BodyPart key={type}>{type}</S.BodyPart>
-    ))}
-  </S.BodyPartList>
-);
+const Bodylist = ({ assets }) => {
+  const [selectedType, setSelectedType] = useState(null);
+  const [emojiList, setEmojiList] = useState({});
+
+  const types = Object.keys(assets);
+
+  const bodyTypeHandler = (value) => {
+    setSelectedType((pState) => (pState === value ? null : value));
+  };
+
+  useEffect(() => {
+    if (!selectedType) return;
+    setEmojiList(assets[selectedType]);
+  }, [assets, selectedType]);
+
+  return (
+    <>
+      <S.BodyPartList>
+        {types.map((type) => (
+          <S.BodyPart
+            key={type}
+            onClick={() => bodyTypeHandler(type)}
+          >{type}
+          </S.BodyPart>
+        ))}
+      </S.BodyPartList>
+      <ul>
+        {emojiList && Object.keys(emojiList).map((each) => <li>{each}</li>)}
+      </ul>
+    </>
+  );
+};
 
 export default Bodylist;
