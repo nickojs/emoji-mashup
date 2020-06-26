cfb8bff84ea2582271b62ed5ff7132b9a00a6a1c
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-26 05:24:57 -0300

    Controls with non infinite useEffect loops

--- src/components/preview/controls/controls.js
+++ src/components/preview/controls/controls.js
@@ -3,10 +3,33 @@ import { useDispatch } from 'react-redux';
 import formConfig from './formConfig';
 
 const Controls = ({ part, emoji }) => {
-  const [currentEmoji, setCurrentEmoji] = useState(null);
+  const [emojiPos, setEmojiPos] = useState(null);
   const [inputs, setInputs] = useState(formConfig);
-
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const object = {
+        xAxis: inputs.xAxis.value,
+        yAxis: inputs.yAxis.value,
+        size: inputs.size.value
+      };
+      setEmojiPos(object);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [inputs]);
+
+  useEffect(() => {
+    const updatedEmoji = {
+      ...emoji[part],
+      position: {
+        ...emoji[part].position,
+        ...emojiPos
+      }
+    };
+    dispatch({ type: part.toUpperCase(), value: updatedEmoji });
+  }, [dispatch, emojiPos]);
 
   const inputChangeHandler = (event, identifier) => {
     const currentForm = { ...inputs };
