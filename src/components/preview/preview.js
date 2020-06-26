193e25396406d71eae5202bb55de009217fe485f
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-26 01:37:43 -0300

    Mocking controls option to manipulate the emoji positioning

--- src/components/preview/preview.js
+++ src/components/preview/preview.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import * as S from './styles';
 
 const Preview = () => {
-  /*
-    emoji is a js object containing the five emoji parts properties:
-    body, effects, eyes, mouth and nose
-  */
+  const [controls, setControls] = useState([]);
   const { emoji } = useSelector((state) => state);
   const emojiKeys = Object.keys(emoji);
 
-  return (
-    <S.PreviewArea>
-      {emojiKeys.map((each) => (
-        <img
-          key={each}
-          src={emoji[each]?.url}
-          alt={emoji[each]?.id}
-        />
-      ))}
+  useEffect(() => {
+    const keys = Object.keys(emoji);
+    const availableControls = keys.filter((each) => emoji[each] !== null);
+    setControls(availableControls);
+  }, [emoji]);
 
-    </S.PreviewArea>
+  return (
+    <>
+      <S.PreviewArea>
+        {emojiKeys.map((each) => (
+          <img
+            key={each}
+            src={emoji[each]?.url}
+            alt={emoji[each]?.id}
+          />
+        ))}
+      </S.PreviewArea>
+      <div>
+        <p>Adjust your emoji:</p>
+        {controls.map((each) => (
+          <div key={each}>
+            <p>{each}</p>
+            <label htmlFor="xAxis">X axis
+              <input name="xAxis" type="range" min="0" max="10" defaultValue="5" step=".2" />
+            </label>
+            <label htmlFor="yAxis">Y axis
+              <input name="yAxis" type="range" min="0" max="10" defaultValue="5" step=".2" />
+            </label>
+            <label htmlFor="size">Scale
+              <input name="size" type="range" min="0" max="10" defaultValue="5" step=".2" />
+            </label>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
