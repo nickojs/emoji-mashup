7c0d9106be5236a203d0d2980b4f78ac06e50bd4
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-27 05:14:47 -0300

    Added more granularity to the inputs

--- src/components/controls/formConfig.js
+++ src/components/controls/formConfig.js
@@ -3,21 +3,21 @@ export default {
     type: 'range',
     min: '0',
     max: '10',
-    step: '.05',
+    step: '.01',
     value: '5'
   },
   yAxis: {
     type: 'range',
     min: '0',
     max: '10',
-    step: '.05',
+    step: '.01',
     value: '5'
   },
   size: {
     type: 'range',
     min: '0',
     max: '2',
-    step: '.05',
+    step: '.01',
     value: '1'
   }
 };
