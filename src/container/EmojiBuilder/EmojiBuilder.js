f59ca36f57233770d06c8ab1ffcbc245ef328b10
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-29 08:12:20 -0300

    Removed controls component

--- src/container/EmojiBuilder/EmojiBuilder.js
+++ src/container/EmojiBuilder/EmojiBuilder.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Preview from '../../components/preview/preview';
 import Bodylist from '../../components/bodylist/bodylist';
-import Controls from '../../components/controls/controls';
 
 const EmojiBuilder = () => (
   <div style={{ maxWidth: '900px', margin: '0 auto' }}>
     <Preview />
-    <Controls />
     <Bodylist />
   </div>
 );
